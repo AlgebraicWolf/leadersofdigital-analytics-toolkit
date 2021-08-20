e422f5ca40e9f591f8c9546695beccbe07608df6
Aleksei Volkov <volkov.aa@phystech.edu>
2021-08-20 21:54:28 +0300

    Added more filters

--- utils/monoidal_filters.py
+++ utils/monoidal_filters.py
@@ -6,14 +6,6 @@ class Filter:
 
   # Composition of two filters  
   def __mul__(self, a):
-    class ComposedFilter(Filter): 
-      def __init__(self, a, b):
-        self.fst = a
-        self.snd = b
-
-      def filt(self, xs: list) -> list:
-        return self.snd.filt(self.fst.filt(xs))
-
     return ComposedFilter(self, a)
 
 # Filter that does nothing 
@@ -29,3 +21,31 @@ class CharacteristicFilter(Filter):
 
   def filt(self, xs: list) -> list:
     return list(filter(self.f, xs))
+
+class ComposedFilter(Filter): 
+  def __init__(self, a, b):
+    self.fst = a
+    self.snd = b
+
+  def filt(self, xs: list) -> list:
+    return self.snd.filt(self.fst.filt(xs))
+
+# Filter that returns only the values that are greater than a given value
+class GEFilter(CharacteristicFilter):
+  def __init__(self, value):
+    CharacteristicFilter.__init__(self, lambda x: x >= value)
+
+# Filter that returns only the values that are less than a given value
+class LEFilter(CharacteristicFilter):
+  def __init__(self, value):
+    CharacteristicFilter.__init__(self, lambda x: x <= value)
+
+# Filter that allows only the values in the given range
+class RangeFilter(ComposedFilter):
+  def __init__(self, lo, hi):
+    ComposedFilter.__init__(self, GEFilter(lo), LEFilter(hi))
+
+# Filter that applies another filter to a given subfield
+class FocusedFilter(CharacteristicFilter):
+  def __init__(self, field: str, F):
+    CharacteristicFilter.__init__(self, lambda x: True if F.filt([x[field]]) else False)
