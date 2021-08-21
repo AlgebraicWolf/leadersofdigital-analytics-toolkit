6880f06adb1572b6b3f07368a22e202cb82bcc77
Aleksei Volkov <volkov.aa@phystech.edu>
2021-08-21 10:51:49 +0300

    Some analytics and pulling comments from instagram posts

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 APP_CONFIG = {
-  'INSTA_LOGIN': <your Instagram login>,
-  'INSTA_PASS': <your Instagram password>,
-  'GAPI_KEY': <your Google API key>,
+  'INSTA_LOGIN': '+79191880851',
+  'INSTA_PASS': 'APackOfWolvesIsHowlingAtMyDoor',
+  'GAPI_KEY': 'AIzaSyBVY4wYBVZ7dglJCcgs-0tpc6kvthCPHm4',
 }
---
+++ sentiment_analysis.py
@@ -0,0 +1,66 @@
+from config import APP_CONFIG
+from enum import Enum
+
+from dostoevsky.tokenization import RegexTokenizer
+from dostoevsky.models import FastTextSocialNetworkModel
+
+from instagrapi import Client
+
+from config import APP_CONFIG
+
+# Class for different sentiments 
+class Sentiment(Enum):
+  POSITIVE = 1
+  NEUTRAL = 2
+  NEGATIVE = 3 
+
+# TODO Pull comments from a YouTube video
+
+# Pull comments from an instagram post
+def pull_instagram_comments(url: str):
+  cl = Client()
+  cl.login(APP_CONFIG['INSTA_LOGIN'], APP_CONFIG['INSTA_PASS'])
+
+  media_id = cl.media_id(cl.media_pk_from_url(url))
+  comments = cl.media_comments(media_id)
+  return list(map(lambda x: x.text, comments))
+
+# Sentiment analysis wrapper
+# Classes are mapped as follows:
+# Positive -> Positive
+# Negative -> Negative
+# Speech, Neutral -> Neutral
+# Skip is not included in the rating 
+def analyze_messages(msgs: list[str]) -> list[Sentiment]:
+  def preprocess_label(labels: str):
+    if labels == 'positive':
+      return Sentiment.POSITIVE
+    elif (labels == 'neutral') or (labels == 'speech'):
+      return Sentiment.NEUTRAL
+    elif labels == 'negative':
+      return Sentiment.NEGATIVE
+    else:
+      return None
+
+  tokenizer = RegexTokenizer()
+  m = FastTextSocialNetworkModel(tokenizer=tokenizer)
+
+  estimates = m.predict(msgs, k=1)
+  labels = map(lambda x: list(x.keys())[0], estimates) # Pull the most likely sentiment classes
+  labels = map(preprocess_label, labels) # Turn strings into labels from Enum
+  labels = filter(lambda x: x is not None, labels) # Drop comments that could not be processed 
+
+  return list(labels)
+
+# Calculate percentage for a given value
+def calculate_percentage(arr, val) -> float:
+  return len(list(filter(lambda x: x == val, arr))) / len(arr)
+
+# Calculate percentage for each class
+def calculate_statistics(msgs: list[Sentiment]) -> dict[str, float]:
+  result = dict()
+  result['positive'] = calculate_percentage(msgs, Sentiment.POSITIVE)
+  result['neutral'] = calculate_percentage(msgs, Sentiment.NEUTRAL)
+  result['negative'] = calculate_percentage(msgs, Sentiment.NEGATIVE)
+
+  return result
